4374ac5c4c6b4810df56c834f23f95cc0446c65e
younghoonJ <to.younghoon.jung@gmail.com>
2021-07-15 22:53:22 +0900

    first

---
+++ jekyll-dev/install_jekyll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+sudo gem install jekyll bundler
---
+++ jekyll-dev/install_node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export NODE_VERSION=14.17.3
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
