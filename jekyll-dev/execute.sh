68032d494ab3bafaf4415702b081bb35d5902afc
younghoonJ <to.younghoon.jung@gmail.com>
2021-07-16 03:02:08 +0900

    f

---
+++ jekyll-dev/execute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/younghoonJ/new_site.git
+cd new_site
+npm install tailwindcss @tailwindcss/typography cssnano postcss postcss-import autoprefixer --save-dev
